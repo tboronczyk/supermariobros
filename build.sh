f6d67765297aad410337ba789a0aa4151af5b214
Timothy Boronczyk <tboronczyk@gmail.com>
2022-02-11 19:49:01 -0500

    update build.sh

--- build.sh
+++ build.sh
@@ -1,23 +1,30 @@
-#! /bin/sh
+#! /bin/bash
 
 set -e
 
-if [ "$#" -ne 1 ]; then
-    echo "usage: $(basename "$0") ROM" 1>&2
-    exit
-fi
-ROM="$1"
+ORIG='Super Mario Bros. (World).nes'
+ROM="supermariobros-eo.nes"
+IPS="supermariobros-eo.ips"
+FLIPS="wine /opt/floating/flips.exe"
 
-# update graphics
-dd if=gfx/8FE0.bin of="$ROM" conv=notrunc bs=1 seek=36832
-dd if=gfx/9210.bin of="$ROM" conv=notrunc bs=1 seek=37392
+echo "GENERATING ROM AND IPS FROM $ORIG..."
+cp "$ORIG" "$ROM"
 
-# update text
-dd if=text/0076.bin of="$ROM" conv=notrunc bs=1 seek=118
-dd if=text/076A.bin of="$ROM" conv=notrunc bs=1 seek=1898
-dd if=text/0792.bin of="$ROM" conv=notrunc bs=1 seek=1938
-dd if=text/079F.bin of="$ROM" conv=notrunc bs=1 seek=1951  # mask for TIME UP
-dd if=text/07B0.bin of="$ROM" conv=notrunc bs=1 seek=1968
-dd if=text/080E.bin of="$ROM" conv=notrunc bs=1 seek=2062
-dd if=text/0D64.bin of="$ROM" conv=notrunc bs=1 seek=3428
-dd if=text/9FC3.bin of="$ROM" conv=notrunc bs=1 seek=40899
+echo "Updating graphics..."
+dd if=gfx/8FE0.bin of="$ROM" conv=notrunc bs=1 seek=$((0x8FE0))
+dd if=gfx/9210.bin of="$ROM" conv=notrunc bs=1 seek=$((0x9210))
+
+echo "Updating text..."
+dd if=text/0076.bin of="$ROM" conv=notrunc bs=1 seek=$((0x0076))
+dd if=text/076A.bin of="$ROM" conv=notrunc bs=1 seek=$((0x076A))
+dd if=text/0792.bin of="$ROM" conv=notrunc bs=1 seek=$((0x0792))
+dd if=text/079F.bin of="$ROM" conv=notrunc bs=1 seek=$((0x079F))  # mask for TIME UP
+dd if=text/07B0.bin of="$ROM" conv=notrunc bs=1 seek=$((0x07B0))
+dd if=text/080E.bin of="$ROM" conv=notrunc bs=1 seek=$((0x080E))
+dd if=text/0D64.bin of="$ROM" conv=notrunc bs=1 seek=$((0x0D64))
+dd if=text/9FC3.bin of="$ROM" conv=notrunc bs=1 seek=$((0x9FC3))
+
+echo "Generating patch..."
+$FLIPS -c -i "$ORIG" "$ROM" "$IPS"
+
+echo "Done"

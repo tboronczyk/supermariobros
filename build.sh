4ec7db35de95fdd2e34e8a767bf7dfe3e58b6f6f
Timothy Boronczyk <tboronczyk@gmail.com>
2022-10-12 19:54:57 -0400

    move files to prg/chr

--- build.sh
+++ build.sh
@@ -10,19 +10,19 @@ FLIPS="wine /opt/floating/flips.exe"
 echo "GENERATING ROM AND IPS FROM $ORIG..."
 cp "$ORIG" "$ROM"
 
-echo "Updating graphics..."
-dd if=gfx/8FE0.bin of="$ROM" conv=notrunc bs=1 seek=$((0x8FE0))
-dd if=gfx/9210.bin of="$ROM" conv=notrunc bs=1 seek=$((0x9210))
+echo "Updating CHR..."
+dd if=chr/1up.bin of="$ROM" conv=notrunc bs=1 seek=$((0x8FE0))
+dd if=chr/cxgxsx.bin of="$ROM" conv=notrunc bs=1 seek=$((0x9210))
 
-echo "Updating text..."
-dd if=text/0076.bin of="$ROM" conv=notrunc bs=1 seek=$((0x0076))
-dd if=text/076A.bin of="$ROM" conv=notrunc bs=1 seek=$((0x076A))
-dd if=text/0792.bin of="$ROM" conv=notrunc bs=1 seek=$((0x0792))
-dd if=text/079F.bin of="$ROM" conv=notrunc bs=1 seek=$((0x079F))  # mask for TIME UP
-dd if=text/07B0.bin of="$ROM" conv=notrunc bs=1 seek=$((0x07B0))
-dd if=text/080E.bin of="$ROM" conv=notrunc bs=1 seek=$((0x080E))
-dd if=text/0D64.bin of="$ROM" conv=notrunc bs=1 seek=$((0x0D64))
-dd if=text/9FC3.bin of="$ROM" conv=notrunc bs=1 seek=$((0x9FC3))
+echo "Updating PRG..."
+dd if=prg/dialog-ptr.bin of="$ROM" conv=notrunc bs=1 seek=$((0x0076))
+dd if=prg/dialog-txt.bin of="$ROM" conv=notrunc bs=1 seek=$((0x0D64))
+dd if=prg/header.bin of="$ROM" conv=notrunc bs=1 seek=$((0x076A))
+dd if=prg/timeup-mask.bin of="$ROM" conv=notrunc bs=1 seek=$((0x079F))
+dd if=prg/gametxt-ptr.bin of="$ROM" conv=notrunc bs=1 seek=$((0x080E))
+dd if=prg/gametxt-text-1.bin of="$ROM" conv=notrunc bs=1 seek=$((0x0792))
+dd if=prg/gametxt-text-2.bin of="$ROM" conv=notrunc bs=1 seek=$((0x07B0))
+dd if=prg/titlescreen.bin of="$ROM" conv=notrunc bs=1 seek=$((0x9FC3))
 
 echo "Generating patch..."
 $FLIPS -c -i "$ORIG" "$ROM" "$IPS"
